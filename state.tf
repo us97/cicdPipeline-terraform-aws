dbfb604143d051c26a0e2e5152832d4ea8c3284c
us97 <uzair.shehzad.23897@gmail.com>
2022-08-14 22:37:36 -0500

    provider added

--- state.tf
+++ state.tf
@@ -1,8 +1,17 @@
 terraform {
+    
   backend"s3" {
         bucket = "test-aws-cicd-pipeline-terraform-state-uzair"
         encrypt = true
         key = "terraform.tfstate"
         region = "us-east-1"
+        config ={
+
+        }
   }
+}
+
+provider "aws" {
+  access_key = "AKIAXK73N2D7YZ3ELCM2"
+  secret_key = "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"
 }
